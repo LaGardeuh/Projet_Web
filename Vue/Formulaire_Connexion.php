129047e95346c04d9b6caf6fd1f3bf19645a2d13
TortueHardie <colin.lienard@free.fr>
2023-03-24 10:43:55 +0100

    Page formulaire connexion

---
+++ Vue/Formulaire_Connexion.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Connexion</title>
+        <link rel="stylesheet" type="text/css" href="..\CSS\Formulaire_Connexion_style.css">    
+    </head>
+
+    <body>
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Inscription</title>
+        </head> 
+    
+        <body>
+            <div class="colonne">
+                <div class="container">
+                    <div class = "header">
+                    <img src="..\Image\logo_cesi_ton_stage.jpg" alt="logo">    
+                </div>
+            <h1>Connectez vous !</h1>
+
+            <form method="POST" action="Formulaire_Connexion.php">
+ 
+            <label for="email">Mail:</label>
+                <input type="email" id="email" name="email" required><br><br>
+
+                <label for="password">Mot de passe:</label>
+                <input type="password" id="password" name="password" required><br><br>
+
+                <input type="submit" value="Connexion">
+
+                <p>Vous n'avez pas de compte ? <a href="Formulaire_Create.php" class="hyperlink" type="hyperlink">Inscription</a></p>
+            </form>
+        </body>
+
+    </html>    
+    </body>
+
+
+</html>
+
