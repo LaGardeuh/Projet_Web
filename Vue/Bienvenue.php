a9a9737ee77f16e9e2c5e6d3bb1d2f3b4ee1606e
AkiiraFPS <mduval44570@gmail.com>
2023-03-24 11:30:49 +0100

    Correctif header

--- Vue/Bienvenue.php
+++ Vue/Bienvenue.php
@@ -1,22 +1,23 @@
-<?php include('header.php'); ?>
-
-<body>
-<section class="afficher_entreprise">
-     <?php
-         if ($all_enterprise->rowCount() > 0){
-             while($enterprise = $all_enterprise->fetch()){
-                ?>
-                <main><?php echo $enterprise['off_entreprise']; ?></main>
-                <?php
+<hmtl>
+    <head>
+    <?php include('header.php'); ?>
+    </head>
+    <body>
+        <section class="afficher_entreprise">
+        <?php
+            if ($all_enterprise->rowCount() > 0){
+                 while($enterprise = $all_enterprise->fetch()){
+                 ?>
+                 <main><?php echo $enterprise['off_entreprise']; ?></main>
+                 <?php
                 }
-        }else{
-            ?>
-            <main>Aucune entreprise trouvé</main>
-            <?php
+            }else{  
+              ?>
+              <main>Aucune entreprise trouvé</main>
+              <?php
             }
-    ?>
- </section>
-<?php include('footer.php'); ?>
-</body>
-
+        ?>
+        </section>
+    <?php include('footer.php'); ?>
+    </body>
 </html>
