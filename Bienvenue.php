0eb8ffa97fb7d7b1c38a49e3292a1c7c40cb65f7
AkiiraFPS <mduval44570@gmail.com>
2023-03-24 10:36:41 +0100

    Upgrade barre de recherche

--- Bienvenue.php
+++ Bienvenue.php
@@ -1,27 +1,21 @@
 <?php include('header.php'); ?>
 
-<main>
-	<h2>Bienvenue sur notre site !</h2>
-
-	<p>Nous sommes une entreprise spécialisée dans la création de sites web modernes et performants. Nous mettons notre expertise à votre service pour vous aider à réaliser vos projets en ligne.</p>
-
-	<h3>Nos services</h3>
-	<ul>
-		<li>Conception de sites web personnalisés</li>
-		<li>Optimisation pour les moteurs de recherche (SEO)</li>
-		<li>Création de contenus (textes, images, vidéos)</li>
-		<li>Intégration de fonctionnalités avancées (e-commerce, blog, forum, etc.)</li>
-		<li>Maintenance et support technique</li>
-	</ul>
-
-	<p>Nous travaillons avec une équipe de professionnels passionnés et expérimentés pour vous offrir un service de qualité supérieure. Nous sommes à l'écoute de vos besoins et nous engageons à vous fournir une solution adaptée à votre budget et à vos objectifs.</p>
-
-	<h3>Contactez-nous</h3>
-	<p>N'hésitez pas à nous contacter pour discuter de votre projet. Nous sommes disponibles pour répondre à vos questions et vous guider dans toutes les étapes de votre projet web.</p>
-	<p><a href="#">Cliquez ici</a> pour accéder à notre formulaire de contact.</p>
-</main>
-
-
+<body>
+<section class="afficher_entreprise">
+     <?php
+         if ($all_enterprise->rowCount() > 0){
+             while($enterprise = $all_enterprise->fetch()){
+                ?>
+                <main><?php echo $enterprise['off_entreprise']; ?></main>
+                <?php
+                }
+        }else{
+            ?>
+            <main>Aucune entreprise trouvé</main>
+            <?php
+            }
+    ?>
+ </section>
 <?php include('footer.php'); ?>
 </body>
 
--- header.php
+++ header.php
@@ -1,3 +1,12 @@
+<?php
+$bdd = new PDO('mysql:host=localhost;dbname=bddweb;','root','');
+$all_enterprise = $bdd->query('SELECT * FROM offre ORDER BY off_id DESC');
+if(isset($_GET['s']) AND !empty($_GET['s'])){
+    $recherche = htmlspecialchars($_GET['s']);
+    $all_enterprise = $bdd->query('SELECT off_entreprise FROM offre WHERE off_entreprise LIKE "%'.$recherche.'%" ORDER BY off_id DESC ');
+}
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -9,8 +18,6 @@
 </head>
 
 <body>
-    </head>
-
     <body>
         <header>
             <!-- <div class="search-bar">
@@ -55,9 +62,9 @@
                         </div>
                     </div>
 
-                    <form class="d-flex search-bar" role="search" method="get">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline" type="submit">Search</button>
+                    <form class="d-flex search-bar" role="search" method="GET">
+                        <input class="form-control me-2" name = "s" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline" type="submit" name="envoyer">Search</button>
                     </form>
 
                     <div class="btn-group">
@@ -73,8 +80,7 @@
 
                 </div>
             </nav>
-
-
+        
 
         </header>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
