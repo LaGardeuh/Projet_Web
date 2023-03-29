21516624dcf65aa673abaa25fe23fa43f00429b7
Malo KERAUTRET <malo.kerautret@viacesi.fr>
2023-03-29 15:19:40 +0200

    vghbn

--- Vue/Login.php
+++ Vue/Login.php
@@ -19,10 +19,10 @@
         <h1>CESI ton Stage</h1>
         <form method="post" action="..\Controler\Connexion.php">
             <label for="email">Email :</label>
-            <input type="email" id="email" name="email" require="require" value="admin@gmail.com"><br>
+            <input type="email" id="email" name="email" require="require" value=""><br>
 
             <label for="password">Mot de passe :</label>
-            <input type="password" id="password" name="password" value="admin"><br>
+            <input type="password" id="password" name="password" value=""><br>
 
             <input type="submit" name="submit" value="Se connecter">
         </form>
